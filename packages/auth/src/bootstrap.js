1a5f4ef478430ccacec5b1d90b50fd821c17e07c
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2025-09-02 17:06:28 -0400

    implement auth app

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App'
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath]
   });
@@ -13,7 +13,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   }
   
   ReactDOM.render(
-    <App history={history} />,
+    <App history={history} onSignIn={onSignIn} />,
     el
   )
 
@@ -29,7 +29,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 }
 
 if(process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root')
+  const devRoot = document.querySelector('#_auth-dev-root')
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() })
